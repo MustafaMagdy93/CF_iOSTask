a4c0bd462c1a51ef1880f2b2d1c555c7432f4009
MustafaMagdy93 <mumaif@hotmail.com>
2022-01-29 18:45:05 +0200

    Pagination commit

--- Helpers/Controllers/Home/HomeVC.swift
+++ Helpers/Controllers/Home/HomeVC.swift
@@ -8,16 +8,23 @@ class HomeVC: UIViewController{
     @IBOutlet weak var popularCollectionView: UICollectionView!
 
     static var popularMoviesArray = [Results]()
+    var moviesPerPage = 3
+    var limit = 3
+    var paginationPopularMoviesArray = [Results]()
     var topMovies = false
     var favoritedArray = [Results]()
     var refreshControl = UIRefreshControl()
     let nowPlayingCollectionViewflowLayout = UICollectionViewFlowLayout()
     let favoritedCollectionViewflowLayout = UICollectionViewFlowLayout()
     static var movieId: Int?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getPopularMovies()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       cellsRegister()
-        getPopularMovies()
+        cellsRegister()
         refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
         refreshControl.addTarget(self, action: #selector(self.refresh(_:)), for: .valueChanged)
         mainView.addSubview(refreshControl) // not required when using UITableViewController
@@ -55,7 +62,7 @@ extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         switch collectionView {
         case popularCollectionView:
-            return HomeVC.popularMoviesArray.count
+            return paginationPopularMoviesArray.count
             
         default:
             return favoritedArray.count
@@ -67,24 +74,25 @@ extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
         switch collectionView {
         case popularCollectionView:
             let cell = popularCollectionView.dequeue(indexPath: indexPath) as PopularCell
-            let imageString = HomeVC.popularMoviesArray[indexPath.row].posterPath
+            let imageString = paginationPopularMoviesArray[indexPath.row].posterPath
             let urlString = "https://image.tmdb.org/t/p/w500/" + imageString
             let imageURL = URL(string: urlString)
             cell.image.kf.setImage(with: imageURL)
-            cell.moviesNameLabel.text = HomeVC.popularMoviesArray[indexPath.row].title
+            cell.moviesNameLabel.text = paginationPopularMoviesArray[indexPath.row].title
             cell.favoriteButton = {
                 if cell.favButton.currentImage == UIImage(systemName: "star") {
                     cell.favButton.setImage(UIImage(systemName: "star.fill"), for: .normal)
-                    self.favoritedArray.append(HomeVC.popularMoviesArray[indexPath.row])
+                    self.favoritedArray.append(self.paginationPopularMoviesArray[indexPath.row])
                     self.favoritedCollectionView.reloadData()
                 } else {
                     cell.favButton.setImage(UIImage(systemName: "star"), for: .normal)
-                    if let index = self.favoritedArray.firstIndex(of: HomeVC.popularMoviesArray[indexPath.row]) {
+                    if let index = self.favoritedArray.firstIndex(of: self.paginationPopularMoviesArray[indexPath.row]) {
                         self.favoritedArray.remove(at: index)
                         self.favoritedCollectionView.reloadData()
                         }
                 }
             }
+            Hud.dismiss()
             return cell
             
         default:
@@ -100,13 +108,22 @@ extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         switch collectionView {
         case popularCollectionView:
-            HomeVC.movieId = HomeVC.popularMoviesArray[indexPath.row].id
+            HomeVC.movieId = paginationPopularMoviesArray[indexPath.row].id
             let storyboard = UIStoryboard(name: "MovieDetails", bundle: nil)
             let view = storyboard.instantiateViewController(identifier: "MovieDetailsVC") as! MovieDetailsVC
             print(indexPath.row)
             present(view, animated: true, completion: nil)
         default:
             return
+        }
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        Hud.show()
+        if scrollView == popularCollectionView {
+            if (scrollView.contentOffset.x + scrollView.frame.size.width) >= (scrollView.contentSize.width) {
+                setPaginationMovies(moviesPerPage: moviesPerPage)
+            }
         }
     }
 }
@@ -119,8 +136,11 @@ extension HomeVC {
                 switch result {
                 case .success(let response):
                     HomeVC.popularMoviesArray = response?.results ?? []
+                    self.limit = HomeVC.popularMoviesArray.count
+                    for i in 0..<10 {
+                        self.paginationPopularMoviesArray.append(HomeVC.popularMoviesArray[i])
+                    }
                     self.popularCollectionView.reloadData()
-                    self.favoritedCollectionView.reloadData()
                     print("response is : \(HomeVC.popularMoviesArray)")
 
                 case .failure(let error):
@@ -128,4 +148,21 @@ extension HomeVC {
                 }
             }
         }
+    
+    func setPaginationMovies(moviesPerPage: Int) {
+        if moviesPerPage >= limit {
+            return
+        } else if moviesPerPage >= limit - 3 {
+            for i in moviesPerPage..<limit {
+                paginationPopularMoviesArray.append(HomeVC.popularMoviesArray[i])
+            }
+            self.moviesPerPage += 3
+        } else {
+            for i in moviesPerPage..<moviesPerPage + 3 {
+                paginationPopularMoviesArray.append(HomeVC.popularMoviesArray[i])
+            }
+            self.moviesPerPage += 3
+        }
+        popularCollectionView.reloadData()
+    }
 }
